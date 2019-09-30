3e34eddf5e851775137ca2d1e248a3e4cf78cf1b
Rick Brown <ricky.brown.00@gmail.com>
2019-09-30 23:06:48 +0100

    added error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function App() {
-  return <div className='App'>app</div>
+  return (
+    <div className='App'>
+      <h1>Error Boundary</h1>
+      {/* lets create an error! */}
+      {null.map(val => val)}
+    </div>
+  )
 }
 
 export default App
